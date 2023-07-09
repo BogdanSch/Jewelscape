bc9ac02a7f819fc12b39dead30d042027e46f80f
Bohdan <bogmira777@gmail.com>
2023-07-09 22:27:11 +0200

    Add form shandler

--- scripts/script.js
+++ scripts/script.js
@@ -10,12 +10,18 @@ $(function () {
     });
 });
 
-//Form Handler
+//Forms Handler
 let forms = document.querySelectorAll("form.contact-form");
 
 forms.forEach((form) => {
     form.addEventListener("submit", e => {
         e.preventDefault();
+
+        let timeField = form.querySelector("#time");
+        let date = Date.now();
+        let options = { day: '2-digit', month: 'long', year: 'numeric' };
+        timeField.value = date.toLocaleString(undefined, options);
+
         fetch(form.action, {
             method: "Post",
             body: new FormData(form),
