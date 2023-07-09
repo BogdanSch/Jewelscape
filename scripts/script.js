808ca4a0dc7d67563df7294f28eb336c56e46b6c
Bohdan <bogmira777@gmail.com>
2023-07-09 21:30:58 +0200

    Add css animation effect to hero section

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ $(function () {
     $(window).scroll(function () {
         $(".slideanim").each(function () {
             let pos = $(this).offset().top;
-            if (pos < $(window).scrollTop() + 400) {
+            if (pos < $(window).scrollTop() + 700) {
                 $(this).addClass("slide");
             }
         });
