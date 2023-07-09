edbc249239805d60e50aff8066c661e3afc5bc0c
Bohdan <bogmira777@gmail.com>
2023-07-09 22:17:09 +0200

    Add form requests

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,31 @@
 
 //Aos
 AOS.init();
+
 // Fields customization
-$(function() {
+$(function () {
     $(".phone").mask("+38 (999)-999-9999", {
         autoсlear: false
-    });    
-})
+    });
+});
+
+//Form Handler
+let forms = document.querySelectorAll("form.contact-form");
+
+forms.forEach((form) => {
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        fetch(form.action, {
+            method: "Post",
+            body: new FormData(form),
+        }).then((html) => {
+            $("#registration-modal").modal("hide");
+            $("#confirm-modal").modal("show");
+            form.reset();
+        }).catch((error) => {
+            $("#registration-modal").modal("hide");
+            $("#error-modal").modal("show");
+            form.reset();
+        });
+    });
+});
