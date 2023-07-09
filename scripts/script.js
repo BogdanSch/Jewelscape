64bba1bfea6a7ed1aac9c2a26559883fb6b7a5f7
Bohdan <bogmira777@gmail.com>
2023-07-09 21:38:19 +0200

    Add aos effects

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-$(function () {
-    $(window).scroll(function () {
-        $(".slideanim").each(function () {
-            let pos = $(this).offset().top;
-            if (pos < $(window).scrollTop() + 700) {
-                $(this).addClass("slide");
-            }
-        });
-    });
-});
+// $(function () {
+//     $(window).scroll(function () {
+//         $(".slideanim").each(function () {
+//             let pos = $(this).offset().top;
+//             if (pos < $(window).scrollTop() + 700) {
+//                 $(this).addClass("slide");
+//             }
+//         });
+//     });
+// });
